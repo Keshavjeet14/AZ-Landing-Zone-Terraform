07b12886df418d406d284bbd2bc258d774733df0
Keshavjeet Singh <jeet.keshav14@gmail.com>
2026-08-06 01:57:04 +0530

    Fix Terraform backend resource dependency

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_storage_account" "sa" {
   for_each = var.storage_accounts
 
   name                            = each.value.name
-  resource_group_name             = each.value.resource_group_name
-  location                        = each.value.location
+  resource_group_name             = azurerm_resource_group.rg[each.key].name
+  location                        = azurerm_resource_group.rg[each.key].location
   account_tier                    = each.value.account_tier
   account_replication_type        = each.value.account_replication_type
   min_tls_version                 = "TLS1_2"
