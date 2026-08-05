d47e2411d083371988adbe1a0c6934e12d6b8bd3
Keshavjeet Singh <jeet.keshav14@gmail.com>
2026-08-05 11:07:22 +0530

    Format Terraform configuration files

--- child_module/azurerm_key_vault_secret/data.tf
+++ child_module/azurerm_key_vault_secret/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_key_vault" "keyvault" {
-  for_each = var.keyvaultsecret27
+  for_each            = var.keyvaultsecret27
   name                = each.value.key_vault_name
   resource_group_name = each.value.resource_group_name
 }
--- child_module/azurerm_resource_group/main.tf
+++ child_module/azurerm_resource_group/main.tf
@@ -2,6 +2,6 @@ variable "rg27" {}
 
 resource "azurerm_resource_group" "rg_block" {
   for_each = var.rg27
-  name = each.value.name
+  name     = each.value.name
   location = each.value.location
 }
